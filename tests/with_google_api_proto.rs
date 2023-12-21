e9bbcc0bcd1626e6bbd1c383ffb2219bb1166cea
bouzuya <m@bouzuya.net>
2023-12-22 07:03:03 +0900

    Add example for CreateDocumentRequest

---
+++ tests/with_google_api_proto.rs
@@ -0,0 +1,48 @@
+use std::{collections::BTreeMap, str::FromStr as _};
+
+use firestore_path::DocumentName;
+use google_api_proto::google::firestore::v1::{CreateDocumentRequest, Document};
+
+#[test]
+fn test_create_document_request() -> anyhow::Result<()> {
+    for (s, p, c, d) in [
+        (
+            "projects/my-project/databases/(default)/documents/chatrooms/chatroom1",
+            "projects/my-project/databases/(default)/documents",
+            "chatrooms",
+            "chatroom1",
+        ),
+        (
+            "projects/my-project/databases/(default)/documents/chatrooms/chatroom1/messages/message1",
+            "projects/my-project/databases/(default)/documents/chatrooms/chatroom1",
+            "messages",
+            "message1",
+        ),
+    ] {
+        let document_name = DocumentName::from_str(s)?;
+
+        let request = CreateDocumentRequest {
+            parent: document_name
+                .clone()
+                .parent()
+                .parent()
+                .map(|parent_document_name| parent_document_name.to_string())
+                .unwrap_or_else(|| document_name.database_name().to_string()),
+            collection_id: document_name.collection_id().to_string(),
+            document_id: document_name.document_id().to_string(),
+            document: Some(Document {
+                name: "".to_string(),
+                fields: BTreeMap::new(),
+                create_time: None,
+                update_time: None,
+            }),
+            mask: None,
+        };
+
+        assert_eq!(request.parent, p);
+        assert_eq!(request.collection_id, c);
+        assert_eq!(request.document_id, d);
+    }
+
+    Ok(())
+}
